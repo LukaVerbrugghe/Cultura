d808c78837e908953f1aebc0e42bc4389ae39947
LukaVerbrugghe <lotrdetailsys@gmail.com>
2025-07-20 10:33:45 +0200

    XAML Signup

---
+++ prjCultura/prjCultura/Signup.xaml.cs
@@ -0,0 +1,29 @@
+namespace prjCultura;
+
+public partial class Signup : ContentPage
+{
+	public Signup()
+	{
+		InitializeComponent();
+	}
+
+	private async void toggleShowFirstPassword(object sender, EventArgs e)
+	{
+
+	}
+
+	private async void toggleShowSecondPassword(object sender, EventArgs e)
+	{ 
+	
+	}
+
+	private async void btnRegistreren_Clicked(object sender, EventArgs e)
+    { 
+    
+    }
+
+	private async void lblGoToLogin_Tapped(object sender, EventArgs e)
+    { 
+    
+    }
+}
