2f95871312898838a6c9ce3dff308d981b8dc8f6
LukaVerbrugghe <lotrdetailsys@gmail.com>
2025-07-20 10:18:13 +0200

    Registered colours + XAML login

---
+++ prjCultura/prjCultura/Login.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace prjCultura
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void toggleShowPassword(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void lblForgetPassword_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        { 
+        
+        }
+    }
+}
