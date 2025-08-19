39ab56552c77b3a4de8e1f5f5a6f0e3266139e8c
LukaVerbrugghe <lotrdetailsys@gmail.com>
2025-08-19 18:00:39 +0200

    Privacy statement checkbox

--- prjCultura/prjCultura/Signup.xaml.cs
+++ prjCultura/prjCultura/Signup.xaml.cs
@@ -39,4 +39,15 @@ public partial class Signup : ContentPage
 		};
 		await Browser.Default.OpenAsync("https://github.com/LukaVerbrugghe/Cultura/blob/main/LICENSE", options);
     }
+
+	private async void lblViewPrivacyPolicy_Tapped(object sender, TappedEventArgs e)
+    {
+        //open the privacy policy on github
+        BrowserLaunchOptions options = new BrowserLaunchOptions()
+        {
+            LaunchMode = BrowserLaunchMode.SystemPreferred,
+            TitleMode = BrowserTitleMode.Show
+        };
+        await Browser.Default.OpenAsync("https://github.com/LukaVerbrugghe/Cultura/blob/main/PRIVACY.md", options);
+    }
 }
